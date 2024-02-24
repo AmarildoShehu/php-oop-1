e7b15d3caaf8b2005e3b702df14bd44c373c38ac
Amarildo <you@example.com>
2024-02-24 16:56:02 +0100

    added new class director and printed

--- index.php
+++ index.php
@@ -20,34 +20,16 @@ class Director
     }
 }
 
-//proprieta 
-class Movie
-{
-   // Variabili d'istanza
-   public $title;
-   public $genre;
-   public $releaseYear;
+// Istanziamento oggetti Director
+$christopherNolan = new Director("Christopher Nolan", 1970);
+$davidFincher = new Director("David Fincher", 1962);
 
-   // Costruttore
-   public function __construct($title, $genre, $releaseYear) {
-    $this->title = $title;
-    $this->genre = $genre;
-    $this->releaseYear = $releaseYear;
-    }
+// Istanziamento oggetti Movie con direttori
+$inception = new Movie("Inception", "Sci-Fi", 2010, $christopherNolan);
+$provaAPrendermi = new Movie("Prova a prendermi", "Crime", 2002, $christopherNolan);
+$fightClub = new Movie("Fight Club", "Drama", 1999, $davidFincher);
 
-    // Metodo
-    public function displayInfo() {
-        echo "Title: {$this->title}, Genre: {$this->genre}, Release Year: {$this->releaseYear}\n";
-    }
-}
-
-// Istanziamento oggetti Movie
-$inception = new Movie("Inception", "Sci-Fi", 2010);
-$provaAPrendermi = new Movie("Prova a prendermi", "Crime", 2002);
-$fightClub = new Movie("Fight Club", "Drama", 1999);
-
-
-//Stampa 
+// Stampa delle informazioni sui film e sui direttori
 echo "<b>Inception:</b>\n";
 $inception->displayInfo();
 
@@ -57,4 +39,31 @@ $provaAPrendermi->displayInfo();
 echo "\n<b>Fight Club:</b>\n";
 $fightClub->displayInfo();
 
+class Movie
+{
+   // Variabili d'istanza
+   public $title;
+   public $genre;
+   public $releaseYear;
+   // Variabile director
+   public $director;
+
+   // Costruttore
+   public function __construct($title, $genre, $releaseYear, $director)
+   {
+    $this->title = $title;
+    $this->genre = $genre;
+    $this->releaseYear = $releaseYear;
+    // Istanza di Director
+    $this->director = $director;
+   }
+
+    // Metodo
+    public function displayInfo()
+    {
+        echo "Title: {$this->title}, Genre: {$this->genre}, Release Year: {$this->releaseYear}\n";
+        // Chiamata al metodo displayInfo di Director
+        $this->director->displayInfo();
+    }
+}
 ?>
